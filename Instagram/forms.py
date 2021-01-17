1c3793edbaff9973776983ed9901b3cb64efa80a
Preston-Too <toopreston@gmail.com>
2021-01-17 08:50:19 +0300

    added class CommentForm

--- Instagram/forms.py
+++ Instagram/forms.py
@@ -38,3 +38,12 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
